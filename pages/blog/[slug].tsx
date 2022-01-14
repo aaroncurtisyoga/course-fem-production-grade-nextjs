7f7412c0b1b9f021b67a3ce491ae9ff33c081eaa
Aaron Curtis <77994989+aaroncurtisyoga@users.noreply.github.com>
2022-01-14 16:55:06 -0500

    Update the Slug to have a fallback. So, if a route isn't definited in `getStaticPaths`, then try and render the content when `getStaticProps` is hit

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -1,14 +1,18 @@
 import React, { FC } from 'react'
 import hydrate from 'next-mdx-remote/hydrate'
+import renderToString from 'next-mdx-remote/render-to-string'
 import { majorScale, Pane, Heading, Spinner } from 'evergreen-ui'
 import Head from 'next/head'
 import { useRouter } from 'next/router'
+import fs from 'fs'
+import matter from 'gray-matter'
+import path from 'path'
+
 import { Post } from '../../types'
 import Container from '../../components/container'
 import HomeNav from '../../components/homeNav'
-import path from 'path'
-import fs from 'fs'
-import matter from 'gray-matter'
+
+import { posts } from '../../content'
 
 const BlogPost: FC<Post> = ({ source, frontMatter }) => {
   const content = hydrate(source)
@@ -48,7 +52,13 @@ BlogPost.defaultProps = {
   frontMatter: { title: 'default title', summary: 'summary', publishedOn: '' },
 }
 
+/**
+ * Need to get the paths here
+ * then the the correct post for the matching path
+ * Posts can come from the fs or our CMS
+ */
 export function getStaticPaths() {
+  // this fn have no access to anything react
   const postsPath = path.join(process.cwd(), 'posts')
   const filenames = fs.readdirSync(postsPath)
 
@@ -61,13 +71,33 @@ export function getStaticPaths() {
 
   return {
     paths: slugs.map((s) => ({ params: { slug: s.slug } })),
-    // non blocking
     fallback: true,
   }
 }
-/**
- * Need to get the paths here
- * then the the correct post for the matching path
- * Posts can come from the fs or our CMS
- */
+
+export async function getStaticProps({ params }) {
+  // this fn have no access to anything react
+  let post
+  try {
+    const filePath = path.join(process.cwd(), 'posts', params.slug + '.mdx')
+    post = fs.readFileSync(filePath, 'utf-8')
+    console.log('post from try is', post)
+  } catch {
+    const cmsPosts = posts.published.map((p) => matter(p))
+    const match = cmsPosts.find((p) => p.data.slug === params.slug)
+    post = match.content
+    console.log('post from catch is', post)
+  }
+
+  const { data } = matter(post)
+  const mdxSource = await renderToString(post, { scope: data }) // render 2 string so can pass in as source
+
+  return {
+    props: {
+      source: mdxSource,
+      frontMatter: data,
+    },
+  }
+}
+
 export default BlogPost
